61e46b92298533a63ad6839362fb67b6ca8127e6
PankajKTech <pankajkatech@gmail.com>
2022-10-10 16:36:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+setInterval(() => {
+
+let d = new Date()
+let htime = d.getHours()
+let mtime = d.getMinutes()
+let stime = d.getSeconds()
+
+hrotation = 30*htime + mtime/2
+mrotation = 6*mtime
+srotation = 6*stime
+
+
+hours.style.transform = `rotate(${hrotation}deg)`
+minutes.style.transform = `rotate(${mrotation}deg)`
+seconds.style.transform = `rotate(${srotation}deg)`
+
+
+
+   
+}, 1000);
